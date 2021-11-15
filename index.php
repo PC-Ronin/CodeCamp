d41e5107c1d96e6826f610d198146df4afdee9f9
PC-Ronin <94197004+PC-Ronin@users.noreply.github.com>
2021-11-15 13:56:45 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!--
+	Russell Dennis
+	Portfolio: Home
+	
+	updated: 07.07.20
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+	<?php include "_top.php"; ?>
+		
+		<main class="w3-container w3-rest">
+			<h3>Who Am I?</h3>
+			
+			<p>My name Russell Dennis; I am an IT Professional, Student Web Developer,
+				Major Video Game Enthusiast, and, most importantly, a family man to three kids and a lovely wife.
+			</p>
+			
+			<p>In 2014, my interest in IT peaked when I attmpted to upgrade my HP desktop computer. 
+				The end result was a new found interest in learning how to build a high performance computer from parts.
+			</P>
+			
+			<p>
+				In 2015, I enrolled in Gwinnett Technical College as a Networking Major.
+			</p>
+			
+			<p>
+				In 2017, I got my first IT job as an on-site support technician.
+			</p>
+			
+			<p>
+				In 2019, I graduated from Gwinnett Technical College with an Associates of Applied Science in Networking.
+				And immediately enrolled into Middle Georgia State University with the goal to obtain a Bachelors in IT.
+			</p>
+			<br />
+			
+			<h3>My Aspirations</h3>
+			
+			<p>I plan on finishing my Bachelors program with a concentration in Web Development.
+				After which, I plan on moving to northern Georgia and shifting my IT career
+				into Web Development.
+			</p>
+			<br />
+			
+			<h3>Currently Working On:</h3>
+			
+			<ul>
+				<li>Web Programming</li>
+				<li>History of Cumputing</li>
+				<li>Discrete Mathematics</li>
+				<li>Game Development Project</li>
+			</ul>
+			<br />
+			
+		</main><!-- END MAIN -->
+		</div><!-- END ROW -->
+		
+	<?php include "_bottom.php"; ?>
+	</body>
+</html>			
